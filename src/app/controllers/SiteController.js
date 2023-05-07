1d6b01c6d8f41edb77f01788ed6cdb4c4e276b69
d2412916 <nguyenvandung86420@gmail.com>
2023-05-07 22:37:41 +0700

    Read DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,11 +1,17 @@
-const Course = require('../models/Course')
+const Course = require('../models/Course');
+const { mutipleMongooseToObject } = require('../../util/mongoose')
+
 class SiteController {
 
 
     //GET /
     index(req, res, next) {
         Course.find({})
-            .then(courses => res.json(courses))
+            .then(courses => {
+                res.render('home', {
+                    courses: mutipleMongooseToObject(courses)
+                });
+            })
             .catch(error => next.error);
     }
 
@@ -13,6 +19,6 @@ class SiteController {
     search(req, res) {
         res.render('search');
     }
-} 
+}
 
-module.exports = new SiteController;
+module.exports = new SiteController();
